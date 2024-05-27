9d26baa609bc84d390801628774b0e4cb8a4fb6c
annaspolavori <spolavorianna@gmail.com>
2024-05-27 16:24:26 -0300

    projeto do jogo

--- JogoDaAdivinhacao/src/jogodaadivinhacao/JogoDaAdivinhacao.java
+++ JogoDaAdivinhacao/src/jogodaadivinhacao/JogoDaAdivinhacao.java
@@ -5,6 +5,9 @@
  */
 package jogodaadivinhacao;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author aluno.saolucas
@@ -15,7 +18,31 @@ public class JogoDaAdivinhacao {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         Scanner obj = new Scanner(System.in);
+        Random random = new Random();
+
+        int numero = 0;
+        int tentativas = 0;
+        int aleatorio = random.nextInt(10);
+
+        while (numero != aleatorio) {
+            tentativas ++;
+            System.out.println("Qual seu palpite?");
+            numero = obj.nextInt();
+
+            if (numero == aleatorio) {
+                System.out.println("Você acertou! Parabéns");
+
+            } else if (numero < aleatorio) {
+                System.out.println("Tente um número maior");
+
+            } else if (numero > aleatorio) {
+                System.out.println("Tente um número menor");
+            }
+            System.out.println("Você tentou " + tentativas + " vezes");
+        }
+
+        
     }
     
 }
